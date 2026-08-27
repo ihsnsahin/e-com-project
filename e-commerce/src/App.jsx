06b1c125f88ffc9e31830cfc0ef7608e046cce5e
ihsnsahin <ihsansahin777@gmail.com>
2026-08-27 20:08:14 +0300

    T01 deploy hariç tamamlandı

---
+++ e-commerce/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 class="bg-amber-900 text-5xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
